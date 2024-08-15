0c5aed51d601ab66148a7cdf3dd8a810bb297374
chenjiaj109550158 <chenjiaj.cs09@nycu.edu.tw>
2024-08-15 15:41:06 +0000

    first_commit

---
+++ run_offload.py
@@ -0,0 +1,130 @@
+import torch
+# from transformers import LlamaForCausalLM, AutoTokenizer
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import argparse
+import time
+
+from models import NaiveWrapper, HuggingFaceWrapper
+
+from accelerate import init_empty_weights, load_checkpoint_and_dispatch, infer_auto_device_map
+
+def main(args):
+    # deterministic
+    torch.manual_seed(0)
+
+    print("Loading model...")
+
+    # load tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(args.llm_path, use_fast=False)
+
+    # load LLM (use init_empty_weights in offload case)
+    with init_empty_weights():
+        llm = AutoModelForCausalLM.from_pretrained(
+            args.llm_path,
+            torch_dtype=torch.float16,
+            low_cpu_mem_usage=True,
+            device_map="auto"
+        )
+
+    if args.mode == "naive":
+        model = NaiveWrapper()
+    elif args.mode == "huggingface" or args.mode == "hf":
+        model = HuggingFaceWrapper()
+    else:
+        raise ValueError("Invalid mode.")
+    
+    # set model
+    model.set_tokenizer(tokenizer)
+    model.set_llm(llm)
+    model.eval()
+
+    print("Warming up model...")
+
+    # input message
+    system_prompt = "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.\n\nIf a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."
+    input_message = "Hello."
+    messages = [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": input_message},
+    ]
+    input_ids = tokenizer.apply_chat_template(messages, tokenize=True, add_generation_prompt=True, return_tensors="pt").cuda()
+    _  = model.generate(input_ids, temperature=args.temp, max_length=args.max_new_token, do_sample=args.do_sample)
+
+    # generate response
+    print("Generating response...")
+
+    # input message
+    system_prompt = "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.\n\nIf a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."
+    input_message = "What's the best way to start learning a new language?"
+    messages = [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": input_message},
+    ]
+    input_ids = tokenizer.apply_chat_template(messages, tokenize=True, add_generation_prompt=True, return_tensors="pt").cuda()
+    prompt = tokenizer.decode(input_ids[0])
+    
+    start_time = time.time()
+    output_ids = model.generate(input_ids, temperature=args.temp, max_length=args.max_new_token, do_sample=args.do_sample)
+    end_time = time.time()
+    
+    output = model.tokenizer.decode(output_ids[0][input_ids.shape[1]:])
+
+    if not args.no_print_message:
+        print("\nPrompt:")
+        print(prompt)
+        print("\nModel response:")
+        print(output)
+        print("\n-----------------------------------")
+        print("Input tokens:", len(input_ids[0]))
+        print("Output tokens:", len(output_ids[0][input_ids.shape[1]:]))
+    
+    if not args.no_print_time:
+        print("Time:", end_time - start_time)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--max-new-token",
+        type=int,
+        default=1024,
+        help="The maximum number of new generated tokens.",
+    )
+    parser.add_argument(
+        "--temp",
+        type=float,
+        default=0.6,
+        help="The temperature for sampling.",
+    )
+    parser.add_argument(
+        "--llm-path",
+        "-llm",
+        type=str,
+        default="meta-llama/Llama-2-7b-chat-hf",
+        help="LLM model path.",
+    )
+    parser.add_argument(
+        "--do-sample",
+        action="store_true",
+        help="Whether to do sampling. (Default is False)",
+    )
+    parser.add_argument(
+        "--mode",
+        type=str,
+        default="naive",
+        help="The mode of model generation.",
+    )
+    parser.add_argument(
+        "-nm",
+        "--no-print-message",
+        action="store_true",
+        help="Print the message.",
+    )
+    parser.add_argument(
+        "-nt",
+        "--no-print-time",
+        action="store_true",
+        help="Record the time.",
+    )
+    args = parser.parse_args()
+    
+    main(args)
