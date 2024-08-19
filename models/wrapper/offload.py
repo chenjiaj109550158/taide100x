09f5e02bd8fa6775044e2c98407a32444d49a158
chenjiaj109550158 <chenjiaj.cs09@nycu.edu.tw>
2024-08-19 16:20:26 +0800

    feat: add benchmark_tpot()

--- models/wrapper/offload.py
+++ models/wrapper/offload.py
@@ -2,6 +2,7 @@ import torch
 from .base import WrapperBase
 # from accelerate import load_checkpoint_and_dispatch, infer_auto_device_map
 from ..offload_tools import load_checkpoint_and_dispatch, infer_auto_device_map
+from .utils import print_cuda_mem_usage
 
 
 def t(module_name, module):
@@ -11,19 +12,37 @@ def t(module_name, module):
         child_name = f"{module_name}.{child_name}"
         print(f"module_name:{child_name}")
         try:
-            print(f"module._hf_hook: {type(child._hf_hook)}")
+            print(f"module_device:{next(child.parameters()).device}")
         except:
-            print(f"module._hf_hook: None")
+            print(f"module_device:None")
+        # try:
+        #     print(f"module._hf_hook: {type(child._hf_hook)}")
+        # except:
+        #     print(f"module._hf_hook: None")
 
-        try:
-            if child._hf_hook.offload:
-                print(f"module._hf_hook.offload: {child._hf_hook.offload} ************************** ")
-            else:
-                print(f"module._hf_hook.offload: {child._hf_hook.offload}")
-        except:
-            print(f"module._hf_hook.offload: None")
+        # try:
+        #     if child._hf_hook.offload:
+        #         print(f"module._hf_hook.offload: {child._hf_hook.offload} ************************** ")
+        #     else:
+        #         print(f"module._hf_hook.offload: {child._hf_hook.offload}")
+        # except:
+        #     print(f"module._hf_hook.offload: None")
         print('----------------------')
         t(child_name, child)
+
+# def tt(module_name, module):
+#     for child_name, child in module.named_children():
+#         # child_name = f"{llm}.{child_name}" if len(llm) > 0 else child_name
+#         # layers_to_hook.append(child_name)
+#         child_name = f"{module_name}.{child_name}"
+#         if child_name != "model.layers.31" and "model.layers.31" in child_name:
+#             print(f"child_name: {child_name}")
+#             if "model.layers.31.mlp" not in child_name:
+#                 child.to("meta")
+#             # else:
+#             #     child.to("cpu")
+
+#         tt(child_name, child)
 
 def print_child_module_names(module):
     for child_name, child in module.named_children():
@@ -32,18 +51,23 @@ def print_child_module_names(module):
         child_name = f"{child_name}"
         print(f"module_name:{child_name}")
         try:
-            print(f"module._hf_hook: {type(child._hf_hook)}")
+            print(f"module_device:{next(child.parameters()).device}")
         except:
-            print(f"module._hf_hook: None")
+            print(f"module_device:None")
 
-        try:
-            if child._hf_hook.offload:
-                print(f"module._hf_hook.offload: {child._hf_hook.offload} ************************** ")
-            else:
-                print(f"module._hf_hook.offload: {child._hf_hook.offload}")
+        # try:
+        #     print(f"module._hf_hook: {type(child._hf_hook)}")
+        # except:
+        #     print(f"module._hf_hook: None")
 
-        except:
-            print(f"module._hf_hook.offload: None")
+        # try:
+        #     if child._hf_hook.offload:
+        #         print(f"module._hf_hook.offload: {child._hf_hook.offload} ************************** ")
+        #     else:
+        #         print(f"module._hf_hook.offload: {child._hf_hook.offload}")
+
+        # except:
+        #     print(f"module._hf_hook.offload: None")
         print('----------------------')
         t(child_name, child)
 
@@ -56,16 +80,17 @@ class OffloadWrapper(WrapperBase):
         device_map = infer_auto_device_map(
             llm,
             max_memory={
-                0: "8GiB",
-                "cpu": "22GiB"
+                0: "4GiB",
+                "cpu": "26GiB"
             },
             no_split_module_classes=["LlamaDecoderLayer"],
             dtype=torch.float16,
         )
 
         layers_to_be_hooked = list(device_map.keys())
-        print(device_map)
+        # print(device_map)
         
+        print("---------------------------------------------------------------------------------------------")
         self.llm = load_checkpoint_and_dispatch(
             llm,
             checkpoint="C:\\Users\\JustinChen\\.cache\\huggingface\\hub\\models--meta-llama--Llama-2-7b-chat-hf\\snapshots\\f5db02db724555f92da89c216ac04704f23d4590\\model.safetensors.index.json", 
@@ -73,9 +98,10 @@ class OffloadWrapper(WrapperBase):
             device_map=device_map,
             # no_split_module_classes=["LlamaDecoderLayer"],
             dtype=torch.float16,
-            layers_to_be_hooked = layers_to_be_hooked
+            # layers_to_be_hooked = layers_to_be_hooked
         )
-        # print_child_module_names(self.llm)
+        # print_cuda_mem_usage()
+        print_child_module_names(self.llm)
         # exit()
         
     @torch.no_grad()
@@ -101,3 +127,13 @@ class OffloadWrapper(WrapperBase):
         )
 
 
+    
+    @torch.no_grad()
+    def run(self, 
+        input_ids: torch.LongTensor, 
+        past_key_values=None,
+    ):
+        return self.llm(
+            input_ids=input_ids,
+            past_key_values=past_key_values,
+        )
--- run_offload.py
+++ run_offload.py
@@ -3,13 +3,73 @@ import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM, AutoConfig
 import argparse
 import time
+from accelerate import init_empty_weights
+import numpy as np
+import os
+import logging
+from transformers.cache_utils import DynamicCache
+
 
 from models import NaiveWrapper, HuggingFaceWrapper, OffloadWrapper
-
-from accelerate import init_empty_weights
-
 # python run_offload.py --mode ol -s --max-new-token 5
 # python run_offload.py --mode ol 
+
+
+logging.getLogger().setLevel(logging.INFO)
+# allocating 40MB to match L2 cache size on A100
+x = torch.empty(int(40 * (1024 ** 2)), dtype=torch.int8, device='cuda')
+def flush_cache():
+    x.zero_()
+
+def prepare_data(config, batch_size, prev_tokens, new_tokens, dtype=torch.float16, device="cuda"):
+    head_dim = config.hidden_size // config.num_attention_heads
+    past_key_values = DynamicCache()
+    for i in range(0, config.num_hidden_layers):
+        cache_k = torch.randn(batch_size, config.num_attention_heads, prev_tokens, head_dim, dtype=dtype, device=device)
+        cache_v = torch.randn(batch_size, config.num_attention_heads, prev_tokens, head_dim, dtype=dtype, device=device)
+        past_key_values.update(cache_k, cache_v, i)
+    
+    tokens = torch.randint(100, (batch_size, new_tokens), device=device)
+    return past_key_values, tokens
+
+@torch.no_grad() # Time per output token
+def benchmark_tpot(model, past_key_values, tokens, repetitions=100):
+    # Get the number of previous tokens
+    prev_tokens = past_key_values.get_seq_length()
+
+    # Warmup steps
+    s = torch.cuda.Stream()
+    s.wait_stream(torch.cuda.current_stream())
+    with torch.cuda.stream(s):
+        for _ in range(repetitions):
+            _ = model.run(tokens, past_key_values=past_key_values)
+            past_key_values.crop(prev_tokens)
+    torch.cuda.current_stream().wait_stream(s)
+    
+    # Capture CUDA graph
+    graph = torch.cuda.CUDAGraph()
+    with torch.cuda.graph(graph):
+        _ = model.run(tokens, past_key_values=past_key_values)
+    # actually not required to crop past_key_values, since cudagraph will replay and read and write to the same memory locations
+    # past_key_values.crop(prev_tokens)
+    
+    # Start and end events
+    start_events = [torch.cuda.Event(enable_timing=True) for _ in range(repetitions)]
+    end_events = [torch.cuda.Event(enable_timing=True) for _ in range(repetitions)]
+    
+    # Run the benchmark
+    for i in range(repetitions):
+        flush_cache()
+        start_events[i].record()
+        graph.replay() 
+        # _ = model.run(tokens, past_key_values=past_key_values)
+        end_events[i].record()
+    torch.cuda.synchronize()
+    
+    times = [s.elapsed_time(e) for s, e in zip(start_events, end_events)]
+    latency = np.median(times) # median is more robust to outliers
+    
+    return latency
 
 def main(args):
     # deterministic
@@ -35,6 +95,7 @@ def main(args):
             device_map="auto"
         )  
 
+    # mode
     if args.mode == "naive":
         model = NaiveWrapper()
     elif args.mode == "huggingface" or args.mode == "hf":
@@ -43,7 +104,15 @@ def main(args):
         model = OffloadWrapper()
     else:
         raise ValueError("Invalid mode.")
-    
+
+    # dtype
+    if args.dtype == "float16":
+        dtype = torch.float16
+    elif args.dtype == "float32":
+        dtype = torch.float32
+    else:
+        raise ValueError(f"Invalid dtype: {args.dtype}")
+
     # set model
     model.set_tokenizer(tokenizer)
     model.set_llm(llm)
@@ -67,7 +136,7 @@ def main(args):
         #     with torch.no_grad():
         #         outputs = model.generate(inputs["input_ids"], max_length=50, num_return_sequences=1)
         start_time = time.time()
-        outputs = model.generate(inputs["input_ids"], max_length=args.max_new_token, num_return_sequences=1)
+        outputs = model.generate(inputs["input_ids"], max_length=args.max_new_tokens, num_return_sequences=1)
         end_time = time.time()
 
         if not args.no_print_message:
@@ -84,6 +153,23 @@ def main(args):
         if not args.no_print_time:
             print("Time:", end_time - start_time)
 
+    elif 1:
+        # Test for number of new tokens from 1 to max_new_tokens
+        for prev_tokens in args.prev_tokens:
+            latencies = []
+            for i in range(1, args.max_new_tokens+1):
+                # past_key_values, tokens = prepare_data(model.config, args.batch_size, prev_tokens, i, dtype=dtype, device=args.device) 
+                past_key_values, tokens = prepare_data(model.llm.config, 1, prev_tokens, i, dtype=dtype, device='cuda') 
+                latency = benchmark_tpot(model, past_key_values, tokens, repetitions=args.repetitions)
+                
+                logging.info(f"Finished. \nprevious_tokens: {prev_tokens} \nnew_tokens: {i} \nlatency: {latency:.2f} milliseconds")
+                latencies.append(latency)
+            
+            # convert to numpy array, plot and save
+            latencies = np.array(latencies)
+
+            # save latencies
+            np.save(os.path.join(args.save_folder, f"llm_prev_{prev_tokens}.npy"), latencies)
     else:
         # input message
         system_prompt = "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.\n\nIf a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."
@@ -93,7 +179,7 @@ def main(args):
             {"role": "user", "content": input_message},
         ]
         input_ids = tokenizer.apply_chat_template(messages, tokenize=True, add_generation_prompt=True, return_tensors="pt").cuda()
-        _  = model.generate(input_ids, temperature=args.temp, max_length=args.max_new_token, do_sample=args.do_sample)
+        _  = model.generate(input_ids, temperature=args.temp, max_length=args.max_new_tokens, do_sample=args.do_sample)
 
         # generate response
         print("Generating response...")
@@ -109,7 +195,7 @@ def main(args):
         prompt = tokenizer.decode(input_ids[0])
         
         start_time = time.time()
-        output_ids = model.generate(input_ids, temperature=args.temp, max_length=args.max_new_token, do_sample=args.do_sample)
+        output_ids = model.generate(input_ids, temperature=args.temp, max_length=args.max_new_tokens, do_sample=args.do_sample)
         end_time = time.time()
         
 
@@ -129,7 +215,7 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--max-new-token",
+        "--max-new-tokens",
         type=int,
         default=1024,
         help="The maximum number of new generated tokens.",
@@ -176,6 +262,24 @@ if __name__ == "__main__":
         action="store_true",
         help="the simple mode(only one prompt)",
     )
+    parser.add_argument(
+        "--prev_tokens", 
+        type=int, 
+        nargs="+", 
+        default=[128,256,512,1024,2048,4096]
+    )
+    parser.add_argument(
+        "--repetitions", 
+        "-rep", 
+        type=int, 
+        default=10
+    )
+    parser.add_argument(
+        "--dtype", 
+        type=str, 
+        default="float16"
+    )
+
     args = parser.parse_args()
     
     main(args)
